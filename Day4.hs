4f4c7c14aae84476f049ca8dd90ce2e97531aea2
vdinovi <vito.dinovi@gmail.com>
2022-12-04 12:40:40 -0800

    Day4: part 2

--- Day4.hs
+++ Day4.hs
@@ -11,8 +11,11 @@ split p s =
 pair :: [a] -> (a,a)
 pair [x,y] = (x,y)
  
-within :: (Ord a) => (a,a) -> (a,a) -> Bool
-within (a,b) (x,y) = a >= x && b <= y
+covers :: (Ord a) => (a,a) -> (a,a) -> Bool
+covers (a,b) (x,y) = a >= x && b <= y
+
+disjoint :: (Ord a) => (a,a) -> (a,a) -> Bool
+disjoint (a,b) (x,y) = (a < x && b < x) || (a > y && b > y)
 
 parse :: [Char] -> [[(Int,Int)]]
 parse str = map parsePair . lines $ str
@@ -21,11 +24,15 @@ parse str = map parsePair . lines $ str
 
 part1 :: [Char] -> Int
 part1 str = length . filter contains . parse $ str
-  where contains [x,y] = x `within` y || y `within` x
+  where contains [x,y] = x `covers` y || y `covers` x
+
+part2 :: [Char] -> Int
+part2 str = length . filter overlaps . parse $ str
+  where overlaps [x,y] = not (x `disjoint` y)
 
 main :: IO ()
 main = do
   args <- getArgs
   contents <- readFile . head $ args
   print . part1 $ contents
-
+  print . part2 $ contents

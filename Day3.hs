0815a18f11ed0b00ce02617ccd46c111f1b95c32
vdinovi <vito.dinovi@gmail.com>
2022-12-03 17:19:18 -0800

    Day3: part 2

--- Day3.hs
+++ Day3.hs
@@ -14,21 +14,36 @@ priority' o
   | inRange (65, 90) o  = (o - 64 + 26)
 
 -- assumes string is even length
-split :: [Char] -> ([Char], [Char])
-split str = splitAt (div (length str) 2) str
+divide :: [Char] -> ([Char], [Char])
+divide str = splitAt (div (length str) 2) str
 
-parse :: [Char] -> Int
-parse line = sum . map priority $ dups
+divideAsList :: [Char] -> [[Char]]
+divideAsList str = [fst xs, snd xs]
   where
-    xs = split line
-    dups = nub (intersect (fst xs) (snd xs))
+    xs = divide str
+
+-- assumes list is is divisible by n
+chunkInto :: Int -> [a] -> [[a]]
+chunkInto _ [] = []
+chunkInto n xs = as : chunkInto n bs 
+  where (as,bs) = splitAt n xs
 
 part1 :: [Char] -> Int
-part1 input = sum . map parse . lines $ input
+part1 input = sum . map part1' . lines $ input
+
+part1' :: [Char] -> Int
+part1' line = sum . map priority . nub . foldr1 intersect $ divideAsList line
+
+part2 :: [Char] -> Int
+part2 input = sum . map part2'. chunkInto 3 . lines $ input
+
+part2' :: [[Char]] -> Int
+part2' lines = sum . map priority . nub . foldr1 intersect $ lines
 
 main :: IO ()
 main = do
   args <- getArgs
   contents <- readFile . head $ args
   print . part1 $ contents
- 
+  print . part2 $ contents
+

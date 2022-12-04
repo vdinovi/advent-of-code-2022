b4da281b02b3b8ac4da4d6f9594f1e6a0666c9d6
vdinovi <vito.dinovi@gmail.com>
2022-12-03 17:29:23 -0800

    Day 3: refactor
    implement divide using splitEvery

--- Day3.hs
+++ Day3.hs
@@ -13,29 +13,24 @@ priority' o
   | inRange (97, 122) o = (o - 96)
   | inRange (65, 90) o  = (o - 64 + 26)
 
--- assumes string is even length
-divide :: [Char] -> ([Char], [Char])
-divide str = splitAt (div (length str) 2) str
-
-divideAsList :: [Char] -> [[Char]]
-divideAsList str = [fst xs, snd xs]
-  where
-    xs = divide str
-
 -- assumes list is is divisible by n
-chunkInto :: Int -> [a] -> [[a]]
-chunkInto _ [] = []
-chunkInto n xs = as : chunkInto n bs 
+splitEvery :: Int -> [a] -> [[a]]
+splitEvery _ [] = []
+splitEvery n xs = as : splitEvery n bs 
   where (as,bs) = splitAt n xs
+
+-- assumes list is divisible by 2
+divide :: [a] -> [[a]]
+divide xs = splitEvery (div (length xs) 2) xs
 
 part1 :: [Char] -> Int
 part1 input = sum . map part1' . lines $ input
 
 part1' :: [Char] -> Int
-part1' line = sum . map priority . nub . foldr1 intersect $ divideAsList line
+part1' line = sum . map priority . nub . foldr1 intersect $ divide line
 
 part2 :: [Char] -> Int
-part2 input = sum . map part2'. chunkInto 3 . lines $ input
+part2 input = sum . map part2'. splitEvery 3 . lines $ input
 
 part2' :: [[Char]] -> Int
 part2' lines = sum . map priority . nub . foldr1 intersect $ lines

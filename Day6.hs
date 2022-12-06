47dd37129d4280b11f2098f9f6a123d501261fc6
vdinovi <vito.dinovi@gmail.com>
2022-12-05 23:31:09 -0800

    Day6: part 2

--- Day6.hs
+++ Day6.hs
@@ -7,18 +7,26 @@ search :: (Eq a) => Int -> [a] -> [a] -> Maybe Int
 search _ [] _ = Nothing
 search _ window [] = Nothing
 search n window (x:xs)
-  | (==) 5 . length . nub $ x:window = Just n 
+  | (<) (length window) . length . nub $ x:window = Just n
   | otherwise = search (n+1) (x:init window) xs
 
-part1 :: [Char] -> Int
-part1 str = n 
-  where (window,rest) = splitAt 4 str
-        n = case search 4 (reverse window) rest of
+solve :: Int -> [Char] -> Int
+solve len str = n
+  where (window,rest) = splitAt len str
+        n = case search len (reverse window) rest of
           Just n -> n
           Nothing -> error "No signal, just noise"
+
+part1 :: [Char] -> Int
+part1 str = solve 4 str
+
+part2 :: [Char] -> Int
+part2 str = (solve 13 str) + 1
 
 main :: IO ()
 main = do
   args <- getArgs
   contents <- readFile . head $ args
   print . part1 $ contents
+  print . part2 $ contents
+
